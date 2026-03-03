68ec3077135afdcc1ad78432d2c746e904b7eb4b
RPrashan99 <RashmithaMK@gmail.com>
2026-03-03 09:58:53 +0530

    embeddings and vector store added, gitignore updated

--- rag-pdf-assistant/ingest.py
+++ rag-pdf-assistant/ingest.py
@@ -1,16 +1,27 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
 
+#Load PDF document
 loader = PyPDFLoader("data/Individual contribution.pdf")
 documents = loader.load()
 
 print("Length: ", len(documents))
 
+#Split text to chunks
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=500,
     chunk_overlap=100
 )
 
 chunks = text_splitter.split_documents(documents)
-
 print("Chunks: ", len(chunks))
+
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+vectorstore = FAISS.from_documents(chunks, embeddings)
+vectorstore.save_local("faiss_index")
+print("Index saved locally.")
