b06739beabad2cc1ca06a3417f8885e74af629a1
RPrashan99 <RashmithaMK@gmail.com>
2026-03-03 09:52:13 +0530

    chunks creating added and requirements updated for newest versions

--- rag-pdf-assistant/ingest.py
+++ rag-pdf-assistant/ingest.py
@@ -1,7 +1,16 @@
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 loader = PyPDFLoader("data/Individual contribution.pdf")
 documents = loader.load()
 
-print(len(documents))
-print(documents[0].page_content[:300])
+print("Length: ", len(documents))
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=500,
+    chunk_overlap=100
+)
+
+chunks = text_splitter.split_documents(documents)
+
+print("Chunks: ", len(chunks))
